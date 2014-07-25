00add38007bdd05cee1e8bd3106f3533c48b84d3
Pongsakorn U-chupala <knightbaron@gmail.com>
2014-07-25 21:22:20 +0900

    Add more comments explaining agent config file

--- agent/config.py
+++ agent/config.py
@@ -1,11 +1,11 @@
-#config file for overlord agent
+# config file for overlord agent
 
-AGENT_HOSTNAME = "fe"
-AGENT_AUTHKEY = "none"
+AGENT_HOSTNAME = "fe" # Must be the same as in DB
+AGENT_AUTHKEY = "none" # Not implemented
 SERVER_LISTEN = "http://fe:8081/listen"
 SERVER_GETJOBS = "http://fe:8081/getjobs"
-INTERVAL = 15
+INTERVAL = 15 # Seconds
 PID_FILE = "/tmp/overlord_agent.pid"
 
-CPU_BUSY = 50
-NET_BUSY = 10240000
+CPU_BUSY = 50 # Percent
+NET_BUSY = 10240000 # Bytes
